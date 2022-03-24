42e54fab1d3b6d95ad42517e97d19a34010b31ab
Yichun <59522685+xieyichun50@users.noreply.github.com>
2022-03-24 20:49:21 +0800

    Create runcafe.sh

---
+++ scripts/runcafe.sh
@@ -0,0 +1,33 @@
+## Get files to the directory
+## In Orthofinder/Results_Feb19/
+ls -l Species_Tree/SpeciesTree_rooted_node_labels.txt
+ls -l MultipleSequenceAlignments/SpeciesTreeAlignment.fa
+ls -l Orthogroups/Orthogroups.GeneCount.tsv
+##cp to cafe working directory
+
+samtools faidx SpeciesTreeAlignment.fa
+cat SpeciesTreeAlignment.fa.fai
+
+python2 /tools/CAFE5/python_scripts/cafetutorial_prep_r8s.py -i SpeciesTree_rooted_node_labels.txt -o r8s_ctl_file.txt -s 484467 -p 'Hydra_vulgaris,Pachyseris_speciosa' -c '617'
+
+awk 'OFS="\t" {$NF=""; print}' Orthogroups.GeneCount.tsv > tmp && awk '{print "(null)""\t"$0}' tmp > cafe.input.tsv && sed -i '1s/(null)/Desc/g' cafe.input.tsv && rm tmp
+python3 /tools/CAFE5/python_scripts/cafetutorial_clade_and_size_filter.py -i cafe.input.tsv -o cafe.input.filter.tsv -s
+
+##run cafe
+echo "/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda1 -c 180"
+/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda1 -c 38
+
+echo "/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda2 -k 2 -c 180"
+/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda2 -k 2 -c 180
+
+echo "/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda3 -k 3 -c 180"
+/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda3 -k 3 -c 180
+
+echo "/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda4 -k 4 -c 180"
+/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda4 -k 4 -c 180
+
+echo "/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda5 -k 5 -c 180"
+/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda5 -k 5 -c 180
+
+echo "/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda6 -k 6 -c 180"
+/tools/CAFE5/bin/cafe5 -i cafe.input.filter.tsv -t r8s.ultrametric.tre -o r8s_filter_lambda6 -k 6 -c 180
