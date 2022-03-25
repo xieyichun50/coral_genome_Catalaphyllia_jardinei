d5bac15646dac1fcbe9ccc43018f87f26c1df3c8
Yichun <59522685+xieyichun50@users.noreply.github.com>
2022-03-25 14:43:16 +0800

    Update runcafe.sh

--- scripts/runcafe.sh
+++ scripts/runcafe.sh
@@ -9,6 +9,9 @@ samtools faidx SpeciesTreeAlignment.fa
 cat SpeciesTreeAlignment.fa.fai
 
 python2 /tools/CAFE5/python_scripts/cafetutorial_prep_r8s.py -i SpeciesTree_rooted_node_labels.txt -o r8s_ctl_file.txt -s 484467 -p 'Hydra_vulgaris,Pachyseris_speciosa' -c '617'
+/tools/r8s/r8s1.81/src/r8s -b -f r8s_ctl_file.txt > r8s_tmp.txt
+tail -n 1 r8s_tmp.txt | cut -c 16- > r8s.ultrametric.tre
+cat r8s.ultrametric.tre
 
 awk 'OFS="\t" {$NF=""; print}' Orthogroups.GeneCount.tsv > tmp && awk '{print "(null)""\t"$0}' tmp > cafe.input.tsv && sed -i '1s/(null)/Desc/g' cafe.input.tsv && rm tmp
 python3 /tools/CAFE5/python_scripts/cafetutorial_clade_and_size_filter.py -i cafe.input.tsv -o cafe.input.filter.tsv -s
