1637c66c32d8f2a82a555a09a70837f486eb7509
Yichun <59522685+xieyichun50@users.noreply.github.com>
2022-04-08 16:12:30 +0800

    Create eggnog_annotation.sh

---
+++ scripts/eggnog_annotation.sh
@@ -0,0 +1,2 @@
+emapper-2.1.2
+emapper.py  --target_orthologs all --go_evidence non-electronic -m diamond --pident 40 --seed_ortholog_evalue 0.001 --seed_ortholog_score 60 --query_cover 20 --subject_cover 20 --override -i Trinity.fasta.transdecoder.pep -o Trinity.fasta.transdecoder.pep.eggnog --output_dir eggnog --temp_dir eggnog_tmp --database none --cpu 38 --target_taxa 33213 --excluded_taxa 7742
